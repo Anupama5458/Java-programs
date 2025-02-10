1a800a1dbd282a01e827c371a89a73aa6d4440d2
Anupama Dubey <117531306+Anupama5458@users.noreply.github.com>
2025-02-10 11:47:57 +0530

    Add files via upload

---
+++ StringEqualDemo.java
@@ -0,0 +1,14 @@
+class StringEqualDemo{
+
+  public static void main(String[] args){
+    String s1="Jaynum";
+    String s2="Jaynum";
+    if(s1==s2){
+     System.out.println("Both strings are equal");
+    }
+else
+   System.out.println("Both strings are not equal");
+
+}
+
+}
